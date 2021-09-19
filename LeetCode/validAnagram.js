ab6c7133084d3500cf0f79fab6b1f25d86a3268b
Haruna Oseni <harunaoseni23@gmail.com>
2021-09-18 20:54:17 -0500

    validAnagram code solution

---
+++ LeetCode/validAnagram.js
@@ -0,0 +1,29 @@
+// VALID ANAGRAM!
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+var validAnagram = function(s,t){
+    let sString = s.split("").sort(); 
+    let tString = s.split("").sort();
+
+    if(tString.length > sString.length){
+        return false;
+    }
+
+    for(let i=0; i<sString.length; i++){
+        if(sString[i] !== tString[i]){
+            return false;
+        }
+    }
+    return true;
+}
