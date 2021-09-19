41420ffd634c29bdc8f4d9030218bc507a379011
Haruna Oseni <harunaoseni23@gmail.com>
2021-09-18 20:46:21 -0500

    3sums codes

--- LeetCode/3sums.js
+++ LeetCode/3sums.js
@@ -4,8 +4,6 @@
 
 // Notice that the solution set must not contain duplicate triplets.
 
- 
-
 // Example 1:
 
 // Input: nums = [-1,0,1,2,-1,-4]
@@ -18,10 +16,31 @@
 
 // Input: nums = [0]
 // Output: []
+var threeSums = function (array) {
+  array.sort((a, b) => a - b);
+  let result = [];
+  if (array.length < 3) {
+    return result;
+  }
 
+  if (i > 0 && array[i] !== array[i - 1]) {
+    for (let i = 0; i < array.length; i++) {
+      let leftPointer = i + 1;
+      let rightPointer = array.length - 1;
+      let brianSum = array[i] + array[leftPointer] + array[rightPointer];
 
-
-
-var threeSums = function(array){
-
-}
+      if (brianSum === 0) {
+        result.push([array[i], array[leftPointer], array[rightPointer]]);
+        while (array[leftPointer] === array[leftPointer + 1]) leftPointer++;
+        while (array[rightPointer] === array[rightPointer - 1]) rightPointer--;
+        leftPointer++;
+        rightPointer++;
+      } else if (brianSum > 0) {
+        rightPointer--;
+      } else if (brianSum < 0) {
+        leftPointer++;
+      }
+    }
+  }
+  return result;
+};
