60bb0f7e8546f8db2fc203ebcf6767a0a3095667
Haruna Oseni <harunaoseni23@gmail.com>
2021-09-19 03:51:46 -0500

    moveZeroes code

---
+++ LeetCode/moveZeroes.js
@@ -0,0 +1,33 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+// Input:
+// [0,0,1]
+// Output:
+// [0,1,0]
+// Expected:
+// [1,0,0]
+
+var moveZeroes = function (nums) {
+    if (nums.length <= 1) {
+      return nums;
+    }
+      
+    for(let i=nums.length-1; i>=0; i--){
+        if(nums[i] === 0){
+            nums.push(0);
+            nums.splice(i,1);
+           }
+    }
+    return nums
+  };
