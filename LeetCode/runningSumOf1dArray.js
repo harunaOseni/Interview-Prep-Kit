2c70a40541d944cc5d4390dd419a564dde278780
Haruna Oseni <harunaoseni23@gmail.com>
2021-10-01 19:44:45 -0500

    runningSum1d code

---
+++ LeetCode/runningSumOf1dArray.js
@@ -0,0 +1,10 @@
+var runningSum = function(nums) {
+    let resultArray = []; 
+    for(let index = 0; index < nums.length; index++){
+        if(index === 0){
+            resultArray.push(nums[i]); 
+        }else{
+            
+        }
+    }
+}
