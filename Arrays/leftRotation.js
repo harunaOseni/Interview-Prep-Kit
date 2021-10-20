4b06b1eb8e22655d4a7577fab3a411b37a645590
Haruna Oseni <harunaoseni23@gmail.com>
2021-10-20 02:16:27 -0500

    deleted files not needed

--- Arrays/leftRotation.js
+++
@@ -1,8 +0,0 @@
-function rotLeft(a, d) {
-  // Write your code here
-  let secondArray = a.slice(d);
-  let firstArray = a.slice(0, d);
-  let resultArray = [...secondArray, ...firstArray];
-
-  return resultArray;
-}
--- Object Oriented Programming/index.js
+++
@@ -1,1 +0,0 @@
-// What is Object Oriented Programming
