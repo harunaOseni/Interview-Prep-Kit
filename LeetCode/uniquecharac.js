41bdc6c1c1c598fde8e6fa713e56e3c604b275e5
Haruna Oseni <harunaoseni23@gmail.com>
2021-11-22 10:18:58 -0600

    restarting LC journey

--- LeetCode/uniquecharac.js
+++
@@ -1,8 +0,0 @@
-var firstUniqChar = function(str) {
-    for (var i=0; i<str.length; i++) {
-      if ( str.indexOf(str[i]) === str.lastIndexOf(str[i]) ) {
-        return i; // repeats
-      }
-    }
-  return -1;
-};
--- LeetCode/validPhoneNumber.js
+++
@@ -1,3 +0,0 @@
-// VALID PHONE NUMBER LEETCODE CHALLENGE
-
-// egrep "/^([0-9]{3} | [0-9]{3}-) [0-9]{3}-[0-9]{4}$/" File.txt
