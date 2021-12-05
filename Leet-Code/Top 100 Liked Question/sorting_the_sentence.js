d98995b4dbec13c8717da8441b2f83fd19dd9781
Haruna Oseni <harunaoseni23@gmail.com>
2021-12-05 00:55:29 -0600

    sorting the sentence javascript

---
+++ Leet-Code/Top 100 Liked Question/sorting_the_sentence.js
@@ -0,0 +1,53 @@
+// SORTING THE SENTENCE
+
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+
+// A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+// For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+// Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+
+// Example 1:
+
+// Input: s = "is2 sentence4 This1 a3"
+// Output: "This is a sentence"
+// Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
+// Example 2:
+
+// Input: s = "Myself2 Me1 I4 and3"
+// Output: "Me Myself and I"
+// Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.
+
+// Constraints:
+
+// 2 <= s.length <= 200
+// s consists of lowercase and uppercase English letters, spaces, and digits from 1 to 9.
+// The number of words in s is between 1 and 9.
+// The words in s are separated by a single space.
+// s contains no leading or trailing spaces.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortSentence = function (s) {
+  let hashTable = {}; //create a hashtable
+  let array = s.split(" "); // change string to array
+  let result = []; // result to output
+
+  array.forEach((word) => {
+    // loop through each word
+    let key = parseInt(word[word.length - 1]); //get each words number as key
+    word = word.substring(0, word.length - 1); //get each words excluding the number
+    hashTable[key] = word; //assigning each key to their respective word
+  });
+
+  for (let i = 1; i <= array.length; i++) {
+    result.push(hashTable[i]); //pushing each word to result array
+  }
+
+  return result.join(" "); //converting result array to string and returning result.
+};
