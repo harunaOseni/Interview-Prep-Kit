143a38f0b034e8a1933c1cdd46e8c533e68910a1
Haruna Oseni <harunaoseni23@gmail.com>
2021-12-04 23:51:20 -0600

    count items matching rule python

---
+++ Leet-Code/Top 100 Liked Question/count_items_matching_a_rule.py
@@ -0,0 +1,56 @@
+# COUNT ITEMS MATCHIGN A RULE
+
+# You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+# The ith item is said to match the rule if one of the following is true:
+
+# ruleKey == "type" and ruleValue == typei.
+# ruleKey == "color" and ruleValue == colori.
+# ruleKey == "name" and ruleValue == namei.
+# Return the number of items that match the given rule.
+
+
+# Example 1:
+
+# Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+# Output: 1
+# Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+# Example 2:
+
+# Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+# Output: 2
+# Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+
+# Constraints:
+
+# 1 <= items.length <= 104
+# 1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+# ruleKey is equal to either "type", "color", or "name".
+# All strings consist only of lowercase letters.
+
+class Solution(object):
+    def countMatches(self, items, ruleKey, ruleValue):
+        """
+        :type items: List[List[str]]
+        :type ruleKey: str
+        :type ruleValue: str
+        :rtype: int
+        """
+        result = 0
+
+        for i in range(len(items)):
+            # items[i] = [typei, colori, namei]
+            if ruleKey == "type":
+                if items[i][0] == ruleValue:
+                    result += 1
+
+            elif ruleKey == "color":
+                if items[i][1] == ruleValue:
+                    result += 1
+
+            else:
+                if ruleKey == "name":
+                    if items[i][2] == ruleValue:
+                        result += 1
+
+        return result
