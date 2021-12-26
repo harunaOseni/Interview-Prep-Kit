4b8f017813e626fa09d3cc00100ddec7a5640e4a
Haruna Oseni <harunaoseni23@gmail.com>
2021-12-25 20:59:14 -0600

    contains duplicate code added js

---
+++ Leet-Code/contains_duplicate.js
@@ -0,0 +1,34 @@
+// CONTAINS DUPLICATE
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  let hashTable = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (hashTable[nums[i]] === undefined) {
+      hashTable[nums[i]] = 1;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+};
