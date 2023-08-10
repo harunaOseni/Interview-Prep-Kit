c51cb103f87b032059fea131ccea9621fa22edd8
Haruna Oseni <harunaoseni23@gmail.com>
2023-08-09 19:36:27 -0500

    isSubsequence js

---
+++ Leet-AlgoM/isSubsequence.js
@@ -0,0 +1,43 @@
+// 392. Is Subsequence
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+
+
+// Constraints:
+
+// 0 <= s.length <= 100
+// 0 <= t.length <= 104
+// s and t consist only of lowercase English letters.
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+    let sPointer = 0;
+    let tPointer = 0;
+
+    while (sPointer < s.length && tPointer < t.length) {
+        if (s[sPointer] === t[tPointer]) {
+            sPointer++;
+            tPointer++;
+        } else {
+            tPointer++;
+        }
+    }
+
+    return sPointer === s.length;
+};
