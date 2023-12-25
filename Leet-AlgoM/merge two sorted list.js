94e7ea4df179e7c6d2c4f80a2ab9899a3d41b1c2
Haruna Oseni <harunaoseni23@gmail.com>
2023-12-25 16:19:56 -0600

    same iterative approach but different naming

--- Leet-AlgoM/merge two sorted list.js
+++ Leet-AlgoM/merge two sorted list.js
@@ -74,3 +74,29 @@ var mergeTwoLists = function (list1, list2) {
 
   return head.next;
 };
+
+// Same iterative approach different naming
+var mergeTwoLists = function (list1, list2) {
+  let head = new ListNode();
+  let current = head;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+
+    current = current.next;
+  }
+
+  if (!list1) {
+    current.next = list2;
+  } else {
+    current.next = list1;
+  }
+
+  return head.next;
+};
