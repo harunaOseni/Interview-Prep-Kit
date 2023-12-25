865bc748597a79fdfdc480c6f77d681d03744d42
Haruna Oseni <harunaoseni23@gmail.com>
2023-12-25 15:17:13 -0600

    rewrote for mastery

--- Leet-AlgoM/longest_common_prefix.js
+++ Leet-AlgoM/longest_common_prefix.js
@@ -28,13 +28,14 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-  if (strs.length === 0) return "";
   let prefix = strs[0];
+
   for (let i = 1; i < strs.length; i++) {
-    while (strs[i].indexOf(prefix) !== 0) {
-      prefix = prefix.slice(0, prefix.length - 1);
+    while (strs[i].indexOf(prefix)) {
+      prefix = prefix.slice();
     }
   }
+
   return prefix;
 };
 
