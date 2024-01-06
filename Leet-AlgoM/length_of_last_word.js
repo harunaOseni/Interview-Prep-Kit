88ad3978898d32aea33fe278cc71e2a666e03709
Haruna Oseni <harunaoseni23@gmail.com>
2024-01-06 02:47:13 -0600

    rewriting algorithm for mastery

--- Leet-AlgoM/length_of_last_word.js
+++ Leet-AlgoM/length_of_last_word.js
@@ -1,7 +1,7 @@
 // 58. Length of Last Word
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
 
-// A word is a maximal 
+// A word is a maximal
 // substring
 //  consisting of non-space characters only.
 
@@ -30,6 +30,12 @@
 // s consists of only English letters and spaces ' '.
 // There will be at least one word in s.
 
+//Example to learn:
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
 /**
  * @param {string} s
  * @return {number}
@@ -49,3 +55,51 @@ var lengthOfLastWord = function (s) {
 
     return length;
 };
+
+// Rewrite for mastery 1:
+var lengthOfLastWord = function (s) {
+    let length = 0;
+    let i = s.length
+
+    while (i >= 0 && s[i] === " ") {
+        i--;
+    }
+
+    while (i >= 0 && s[i] !== " ") {
+        length++;
+        i--;
+    }
+
+    return length;
+}
+
+//Rewrite for mastery 2:
+var lengthOfLastWord = function (s) {
+    let length = 0;
+    let i = s.length - 1;
+
+    while (i >= 0 && s[i] === " ") {
+        i--;
+    }
+
+    while (i >= 0 && s[i] !== " ") {
+        length++;
+        i--;
+    }
+
+    return length;
+};
+
+//Rewrite for mastery 3:
+var lengthOfLastWord = function (s) {
+    let length = 0;
+    let i = s.length - 1;
+
+    while (i >= 0 && s[i] === " ") {
+        i--;
+    }
+
+    while (i >= 0 && s[i] !== " ") {
+
+    }
+}
