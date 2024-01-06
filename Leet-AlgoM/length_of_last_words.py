56bc8db57b1e679fadd631feebc45cbbd737be5e
Haruna Oseni <harunaoseni23@gmail.com>
2024-01-06 04:33:55 -0600

    python version and rewrite for mastery

---
+++ Leet-AlgoM/length_of_last_words.py
@@ -0,0 +1,90 @@
+# 58. Length of Last Word
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+# A word is a maximal
+# substring
+# consisting of non-space characters only.
+
+
+# Example 1:
+
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+# Example 2:
+
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+# Example 3:
+
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of only English letters and spaces ' '.
+# There will be at least one word in s.
+
+# Example to learn:
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+
+def length_of_last_word(s):
+    length = 0
+    i = len(s) - 1
+
+    while i >= 0 and s[i] != " ":
+        i -= 1
+
+    while i >= 0 and s[i] == " ":
+        length -= 1
+        i -= 1
+
+    return length
+
+
+# Rewrite for mastery 1:
+def length_of_last_word(s):
+    length = 0
+    i = len(s) - 1
+
+    while i >= 0 and s[i] != " ":
+        i -= 1
+
+    while i >= 0 and s[i] == " ":
+        i -= 1
+        length += 1
+
+    return length
+
+# Rewrite for mastery 2:
+def length_of_last_word(s):
+    length = 0
+    i = len(s) - 1
+
+    while i >= 0 and s[i] == " ":
+        i -= 1
+
+    while i >= 0 and s[i] == " ":
+        i -= 1
+
+    return length
+
+# Rewrite for mastery 3:
+def length_of_last_word(s):
+    length = 0
+    i = len(s) - 1
+
+    while i >= 0 and s[i] == " ":
+        i -= 1
+
+    while i >= 0 and s[i] != " ":
+        length += 1
+        i -= 1
+
+    return length
