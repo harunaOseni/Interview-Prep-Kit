299daed6e04b49e0d7d8d7a02d4568e95c2d662e
Haruna Oseni <harunaoseni23@gmail.com>
2024-01-12 12:33:16 -0600

    climbing stairs to mastery for javascript

--- Leet-AlgoM/climbing_stairs.js
+++ Leet-AlgoM/climbing_stairs.js
@@ -38,3 +38,35 @@ var climbStairs = function (n) {
 
   return second
 };
+
+// Rewrite to mastery 1: 
+var climbStairs = function (n) {
+  if (n === 1) return 1;
+
+  let first = 1;
+  let second = 2;
+
+  for (let i = 3; i <= n; i++) {
+    let current = first + second;
+    first = second;
+    second = current;
+  }
+
+  return second;
+};
+
+// Rewrite to mastery 2:
+var climbStairs = function (n) {
+  if (n === 1) return 1;
+
+  let first = 1;
+  let second = 2;
+
+  for (let i = 3; i <= n; i++) {
+    let current = first + second;
+    first = second;
+    second = current;
+  }
+
+  return second;
+};
